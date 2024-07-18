bbb283a971f8e98fc818c8383306475e291c3eb3
Jailton Silva <jss1@ifal.edu.br>
2024-07-18 13:39:07 -0300

    add CommandError custom exception

---
+++ src/commands/exceptions.py
@@ -0,0 +1,9 @@
+class CommandError(Exception):
+    def __init__(
+            self,
+            message: str = "The command signature must be "
+                           "<str:command_name> <str:key> <int:flags> <int:exptime> <int:byte_count> [<str:noreply>]\r\n"
+                           "Did you use int for flags, exptime and byte_count parameters?\r\n"
+    ):
+        self.message = message
+        super().__init__(self.message)
